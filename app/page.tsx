06ab0fecb319f2b52de95dcce0616894922ae595
Giuseppe <cresctelli@gmail.com>
2025-11-28 06:58:11 +0100

    Built searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Breadcrumb } from "@/components/Breadcrumb";
 import { Content } from "@/components/Content";
 import { NoteHeading } from "@/components/NoteHeading";
 import { Notes } from "@/components/Notes";
+import { Search } from "@/components/Search";
 import { Sidebar } from "@/components/Sidebar";
 
 export default function page() {
@@ -12,7 +13,7 @@ export default function page() {
       <AddNote />
       <NoteHeading />
       <Sidebar />
-      <div></div>
+      <Search />
       <Content />
       <Notes />
       <Breadcrumb />
---
+++ components/Search.tsx
@@ -0,0 +1,13 @@
+import { SearchIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
+function Search() {
+  return (
+    <div className="bg-secondary flex items-center px-4 gap-4">
+      <SearchIcon className="text-foreground" />
+      <Input className="border-none dark:bg-secondary" placeholder="Search all notes and tags" />
+    </div>
+  );
+}
+
+export { Search };
