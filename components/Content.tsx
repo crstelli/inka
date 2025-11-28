a7c7e887a2b07d1460bbe5b462cd5b10d6492865
Giuseppe <cresctelli@gmail.com>
2025-11-28 09:24:52 +0100

    Implemented placeholder

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,20 +2,23 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import { FloatingMenu } from "@tiptap/react/menus";
+import { Placeholder } from "@tiptap/extensions";
 import StarterKit from "@tiptap/starter-kit";
+
 import { Button } from "./ui/button";
 import { ButtonGroup } from "./ui/button-group";
-import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
-import { DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
 
 function Content() {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<h1>Welcome to Inka</h1>",
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: "Start by writing something...",
+      }),
+    ],
     editorProps: {
       attributes: {
-        class: "p-10",
+        class: "p-12",
       },
     },
 
@@ -25,16 +28,43 @@ function Content() {
   return (
     <div className="row-span-2 w-full overflow-auto">
       {editor && (
-        <FloatingMenu editor={editor} className="-translate-y-1/2">
+        <FloatingMenu editor={editor} className="-translate-y-full">
           <ButtonGroup>
             <ButtonGroup>
-              <Button onClick={() => editor.chain().toggleHeading({ level: 1 }).focus().run()}>H1</Button>
-              <Button onClick={() => editor.chain().toggleHeading({ level: 2 }).focus().run()}>H2</Button>
-              <Button onClick={() => editor.chain().toggleHeading({ level: 3 }).focus().run()}>H3</Button>
+              <Button variant="outline" onClick={() => editor.chain().toggleHeading({ level: 1 }).focus().run()}>
+                Head 1
+              </Button>
+              <Button variant="outline" onClick={() => editor.chain().toggleHeading({ level: 2 }).focus().run()}>
+                Head 2
+              </Button>
+              <Button variant="outline" onClick={() => editor.chain().toggleHeading({ level: 3 }).focus().run()}>
+                Head 3
+              </Button>
             </ButtonGroup>
             <ButtonGroup>
-              <Button onClick={() => editor.chain().focus().toggleBold().run()} aria-label="Heading">
+              <Button
+                variant="outline"
+                className="font-bold"
+                onClick={() => editor.chain().focus().toggleBold().run()}
+                aria-label="Heading"
+              >
                 B
+              </Button>
+              <Button
+                variant="outline"
+                className="italic"
+                onClick={() => editor.chain().focus().toggleItalic().run()}
+                aria-label="Heading"
+              >
+                I
+              </Button>
+              <Button
+                variant="outline"
+                className="line-through"
+                onClick={() => editor.chain().focus().toggleStrike().run()}
+                aria-label="Heading"
+              >
+                S
               </Button>
             </ButtonGroup>
           </ButtonGroup>
