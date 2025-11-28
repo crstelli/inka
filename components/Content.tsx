bf35ded2456d3f887da9726556fda339f404cdbd
Giuseppe <cresctelli@gmail.com>
2025-11-28 08:15:46 +0100

    Implemented TipTap editor

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,5 +1,33 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import { FloatingMenu } from "@tiptap/react/menus";
+import StarterKit from "@tiptap/starter-kit";
+import { Button } from "./ui/button";
+
 function Content() {
-  return <div className="row-span-2">Content</div>;
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Welcome to Inka</p>",
+    editorProps: {
+      attributes: {
+        class: "p-4",
+      },
+    },
+
+    immediatelyRender: false,
+  });
+
+  return (
+    <div className="row-span-2">
+      {editor && (
+        <FloatingMenu editor={editor}>
+          <Button size={"sm"}>H1</Button>
+        </FloatingMenu>
+      )}
+      <EditorContent editor={editor} />
+    </div>
+  );
 }
 
 export { Content };
