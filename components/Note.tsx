db2fbdf95dec9dcaddc9804904d9e5ecb5372a34
Giuseppe <cresctelli@gmail.com>
2025-11-28 07:12:05 +0100

    Built notes list

---
+++ components/Note.tsx
@@ -0,0 +1,10 @@
+function Note() {
+  return (
+    <div className="bg-secondary h-25 rounded-md p-3">
+      <h3 className="font-medium">Welcome to Inka</h3>
+      <span className="text-muted-foreground">Inka is a note taking app with a modern des...</span>
+    </div>
+  );
+}
+
+export { Note };
--- components/Notes.tsx
+++ components/Notes.tsx
@@ -1,5 +1,23 @@
+import { Note } from "./Note";
+
 function Notes() {
-  return <div>Notes</div>;
+  return (
+    <div className="p-4 flex flex-col gap-4 overflow-auto">
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
 }
 
 export { Notes };
