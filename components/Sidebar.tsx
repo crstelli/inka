5f3190bd0a6168583ddc32fd88fd9d8265c9874d
Giuseppe <cresctelli@gmail.com>
2025-11-28 06:50:24 +0100

    Built sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,25 @@
+import { Settings, StickyNote, Tag, Trash } from "lucide-react";
+import { Button } from "./ui/button";
+
 function Sidebar() {
   return (
-    <div className="bg-secondary h-full flex flex-col row-span-3">
-      <span>All Notes</span>
-      <span>Tags</span>
-      <span>Trash</span>
-      <span>Settings</span>
+    <div className="bg-secondary h-full flex flex-col row-span-3 px-4 py-10 gap-2">
+      <Button variant="ghost" className="justify-start">
+        <StickyNote className="rotate-90" />
+        <span>All Notes</span>
+      </Button>
+      <Button variant="ghost" className="justify-start">
+        <Tag />
+        <span>Tags</span>
+      </Button>
+      <Button variant="ghost" className="justify-start">
+        <Trash />
+        <span>Trash</span>
+      </Button>
+      <Button variant="ghost" className="justify-start">
+        <Settings />
+        <span>Settings</span>
+      </Button>
     </div>
   );
 }
