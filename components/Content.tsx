af4a8036c92af58367b83e2ce8b45be96e9e4e85
Giuseppe <cresctelli@gmail.com>
2025-11-28 09:38:48 +0100

    Styled floating menu

--- components/Content.tsx
+++ components/Content.tsx
@@ -29,38 +29,50 @@ function Content() {
     <div className="row-span-2 w-full overflow-auto">
       {editor && (
         <FloatingMenu editor={editor} className="-translate-y-full">
-          <ButtonGroup>
+          <ButtonGroup className="shadow-[0px_0px_50px_5px_#181c1f]">
             <ButtonGroup>
-              <Button variant="outline" onClick={() => editor.chain().toggleHeading({ level: 1 }).focus().run()}>
+              <Button
+                variant="secondary"
+                className="border"
+                onClick={() => editor.chain().toggleHeading({ level: 1 }).focus().run()}
+              >
                 Head 1
               </Button>
-              <Button variant="outline" onClick={() => editor.chain().toggleHeading({ level: 2 }).focus().run()}>
+              <Button
+                variant="secondary"
+                className="border"
+                onClick={() => editor.chain().toggleHeading({ level: 2 }).focus().run()}
+              >
                 Head 2
               </Button>
-              <Button variant="outline" onClick={() => editor.chain().toggleHeading({ level: 3 }).focus().run()}>
+              <Button
+                variant="secondary"
+                className="border"
+                onClick={() => editor.chain().toggleHeading({ level: 3 }).focus().run()}
+              >
                 Head 3
               </Button>
             </ButtonGroup>
             <ButtonGroup>
               <Button
-                variant="outline"
-                className="font-bold"
+                variant="secondary"
+                className="font-bold border"
                 onClick={() => editor.chain().focus().toggleBold().run()}
                 aria-label="Heading"
               >
                 B
               </Button>
               <Button
-                variant="outline"
-                className="italic"
+                variant="secondary"
+                className="italic border"
                 onClick={() => editor.chain().focus().toggleItalic().run()}
                 aria-label="Heading"
               >
                 I
               </Button>
               <Button
-                variant="outline"
-                className="line-through"
+                variant="secondary"
+                className="line-through border"
                 onClick={() => editor.chain().focus().toggleStrike().run()}
                 aria-label="Heading"
               >
