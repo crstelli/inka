b79f75d605491befddd0426cca0771ef68c9199d
Giuseppe <cresctelli@gmail.com>
2025-11-29 06:55:20 +0100

    Implemented a Save button that hides on empty content

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useState } from "react";
+
 import { useEditor, EditorContent } from "@tiptap/react";
 import { Placeholder } from "@tiptap/extensions";
 import StarterKit from "@tiptap/starter-kit";
 
 import { FloatingMenu } from "./FloatingMenu";
+import { Button } from "./ui/button";
 
 function Content() {
+  const [showSave, setShowSave] = useState(false);
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -20,15 +25,20 @@ function Content() {
       },
     },
 
+    onUpdate({ editor }) {
+      setShowSave(!!editor.state.doc.textContent.length);
+    },
+
     immediatelyRender: false,
   });
-
   if (!editor) return null;
 
   return (
-    <div className="row-span-2 w-full overflow-auto">
+    <div className="row-span-2 w-full overflow-auto relative">
       {editor && <FloatingMenu editor={editor} />}
       <EditorContent editor={editor} />
+
+      {showSave && <Button className="absolute top-3 right-3">Save</Button>}
     </div>
   );
 }
