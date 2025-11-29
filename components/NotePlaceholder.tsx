ae75f52aaf431e67570a84cfd5eb5cf0dd4ee93d
Giuseppe <cresctelli@gmail.com>
2025-11-29 06:22:58 +0100

    Implemented note type for Typescript and empty notes placeholder

---
+++ components/NotePlaceholder.tsx
@@ -0,0 +1,7 @@
+function NotePlaceholder() {
+  return (
+    <div className="text-center mt-5 text-muted-foreground">You have no note saved, start by creating your first!</div>
+  );
+}
+
+export { NotePlaceholder };
--- components/Notes.tsx
+++ components/Notes.tsx
@@ -1,21 +1,14 @@
+import type { Note as NoteType } from "@/lib/noteType";
+
 import { Note } from "./Note";
+import { NotePlaceholder } from "./NotePlaceholder";
+
+const notes: NoteType[] = [];
 
 function Notes() {
   return (
     <div className="p-4 flex flex-col gap-4 overflow-auto">
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
+      {notes.length > 0 ? notes.map((n) => <Note key={n.id} />) : <NotePlaceholder />}
     </div>
   );
 }
