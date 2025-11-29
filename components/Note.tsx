616ba1948f78b53e7c40837981b56947e3266efe
Giuseppe <cresctelli@gmail.com>
2025-11-29 09:56:40 +0100

    Added dynamic styling to current active note in notes list

--- components/Note.tsx
+++ components/Note.tsx
@@ -8,14 +8,21 @@ interface Props {
 }
 
 function Note({ note }: Props) {
-  const { setCurrentNote } = useNotes();
+  const { setCurrentNote, currentNote } = useNotes();
+
+  const isActive = currentNote?.id === note.id;
 
   function handleSetNote() {
     setCurrentNote(note);
   }
 
   return (
-    <div onClick={handleSetNote} className="bg-secondary h-25 rounded-md p-3 flex flex-col cursor-pointer">
+    <div
+      onClick={handleSetNote}
+      className={`h-25 rounded-md p-3 border flex flex-col cursor-pointer ${
+        isActive ? "bg-background" : "bg-secondary border-transparent"
+      }`}
+    >
       <h3 className="font-medium">{note.title}</h3>
       <span className="text-muted-foreground">{note.description || "No description provided"}</span>
     </div>
