cd65f4a0af36054f8ba3e47af3554c2cbbb33415
Giuseppe <cresctelli@gmail.com>
2025-11-29 07:42:08 +0100

    Implemented currentNote state to manage editor and notes list

--- components/Note.tsx
+++ components/Note.tsx
@@ -1,12 +1,21 @@
+"use client";
+
 import type { Note } from "@/lib/noteType";
+import { useNotes } from "./notesContext/Provider";
 
 interface Props {
   note: Note;
 }
 
 function Note({ note }: Props) {
+  const { setCurrentNote } = useNotes();
+
+  function handleSetNote() {
+    setCurrentNote(note);
+  }
+
   return (
-    <div className="bg-secondary h-25 rounded-md p-3 flex flex-col cursor-pointer">
+    <div onClick={handleSetNote} className="bg-secondary h-25 rounded-md p-3 flex flex-col cursor-pointer">
       <h3 className="font-medium">{note.title}</h3>
       <span className="text-muted-foreground">{note.description || "No description provided"}</span>
     </div>
--- components/notesContext/Provider.tsx
+++ components/notesContext/Provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { ReactNode } from "react";
+import type { ReactNode, Dispatch, SetStateAction } from "react";
 import type { JSONContent } from "@tiptap/react";
 
 import { createContext, useContext, useState } from "react";
@@ -13,12 +13,16 @@ interface Props {
 interface ContextProps {
   notes: Note[];
   addNote: (content: JSONContent) => void;
+
+  currentNote: Note | undefined;
+  setCurrentNote: Dispatch<SetStateAction<Note | undefined>>;
 }
 
 const NotesContext = createContext<null | ContextProps>(null);
 
 function NotesProvider({ children }: Props) {
   const [notes, setNotes] = useState<Note[]>([]);
+  const [currentNote, setCurrentNote] = useState<Note | undefined>(undefined);
 
   function addNote(content: JSONContent) {
     const note = {
@@ -33,7 +37,9 @@ function NotesProvider({ children }: Props) {
     setNotes((n) => [...n, note]);
   }
 
-  return <NotesContext.Provider value={{ notes, addNote }}>{children}</NotesContext.Provider>;
+  return (
+    <NotesContext.Provider value={{ notes, addNote, currentNote, setCurrentNote }}>{children}</NotesContext.Provider>
+  );
 }
 
 function useNotes() {
