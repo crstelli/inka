f69194df98fb76872c9f6ac7d652bdf324157ce0
Giuseppe <cresctelli@gmail.com>
2025-11-29 06:30:49 +0100

    Exported FloatingMenu in his own file

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { useEditor, EditorContent, useEditorState } from "@tiptap/react";
-import { FloatingMenu } from "@tiptap/react/menus";
 import { Placeholder } from "@tiptap/extensions";
 import StarterKit from "@tiptap/starter-kit";
 
-import { Button } from "./ui/button";
-import { ButtonGroup } from "./ui/button-group";
+import { FloatingMenu } from "./FloatingMenu";
 
 function Content() {
   const editor = useEditor({
@@ -25,83 +23,11 @@ function Content() {
     immediatelyRender: false,
   });
 
-  const editorState = useEditorState({
-    editor,
-
-    selector: ({ editor }) => {
-      if (!editor) return null;
-
-      return {
-        isH1: editor.isActive("heading", { level: 1 }),
-        isH2: editor.isActive("heading", { level: 2 }),
-        isH3: editor.isActive("heading", { level: 3 }),
-
-        isBold: editor.isActive("bold"),
-        isItalic: editor.isActive("italic"),
-        isStrike: editor.isActive("strike"),
-      };
-    },
-  });
-
   if (!editor) return null;
 
   return (
     <div className="row-span-2 w-full overflow-auto">
-      {editor && (
-        <FloatingMenu editor={editor} className="-translate-y-full">
-          <ButtonGroup className="shadow-[0px_0px_50px_5px_#181c1f]">
-            <ButtonGroup>
-              <Button
-                variant={editorState?.isH1 ? "default" : "secondary"}
-                className="border"
-                onClick={() => editor.chain().toggleHeading({ level: 1 }).focus().run()}
-              >
-                Head 1
-              </Button>
-              <Button
-                variant={editorState?.isH2 ? "default" : "secondary"}
-                className="border"
-                onClick={() => editor.chain().toggleHeading({ level: 2 }).focus().run()}
-              >
-                Head 2
-              </Button>
-              <Button
-                variant={editorState?.isH3 ? "default" : "secondary"}
-                className="border"
-                onClick={() => editor.chain().toggleHeading({ level: 3 }).focus().run()}
-              >
-                Head 3
-              </Button>
-            </ButtonGroup>
-            <ButtonGroup>
-              <Button
-                variant={editorState?.isBold ? "default" : "secondary"}
-                className={`font-bold border`}
-                onClick={() => editor.chain().focus().toggleBold().run()}
-                aria-label="Heading"
-              >
-                B
-              </Button>
-              <Button
-                variant={editorState?.isItalic ? "default" : "secondary"}
-                className="italic border"
-                onClick={() => editor.chain().focus().toggleItalic().run()}
-                aria-label="Heading"
-              >
-                I
-              </Button>
-              <Button
-                variant={editorState?.isStrike ? "default" : "secondary"}
-                className="line-through border"
-                onClick={() => editor.chain().focus().toggleStrike().run()}
-                aria-label="Heading"
-              >
-                S
-              </Button>
-            </ButtonGroup>
-          </ButtonGroup>
-        </FloatingMenu>
-      )}
+      {editor && <FloatingMenu editor={editor} />}
       <EditorContent editor={editor} />
     </div>
   );
---
+++ components/FloatingMenu.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import type { Editor } from "@tiptap/react";
+import { FloatingMenu as FloatingMenuApi } from "@tiptap/react/menus";
+import { useEditorState } from "@tiptap/react";
+
+import { Button } from "./ui/button";
+import { ButtonGroup } from "./ui/button-group";
+
+interface Props {
+  editor: Editor;
+}
+
+function FloatingMenu({ editor }: Props) {
+  const editorState = useEditorState({
+    editor,
+
+    selector: ({ editor }) => {
+      if (!editor) return null;
+
+      return {
+        isH1: editor.isActive("heading", { level: 1 }),
+        isH2: editor.isActive("heading", { level: 2 }),
+        isH3: editor.isActive("heading", { level: 3 }),
+
+        isBold: editor.isActive("bold"),
+        isItalic: editor.isActive("italic"),
+        isStrike: editor.isActive("strike"),
+      };
+    },
+  });
+
+  return (
+    <FloatingMenuApi editor={editor} className="-translate-y-full">
+      <ButtonGroup className="shadow-[0px_0px_50px_5px_#181c1f]">
+        <ButtonGroup>
+          <Button
+            variant={editorState?.isH1 ? "default" : "secondary"}
+            className="border"
+            onClick={() => editor.chain().toggleHeading({ level: 1 }).focus().run()}
+          >
+            Head 1
+          </Button>
+          <Button
+            variant={editorState?.isH2 ? "default" : "secondary"}
+            className="border"
+            onClick={() => editor.chain().toggleHeading({ level: 2 }).focus().run()}
+          >
+            Head 2
+          </Button>
+          <Button
+            variant={editorState?.isH3 ? "default" : "secondary"}
+            className="border"
+            onClick={() => editor.chain().toggleHeading({ level: 3 }).focus().run()}
+          >
+            Head 3
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button
+            variant={editorState?.isBold ? "default" : "secondary"}
+            className={`font-bold border`}
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            aria-label="Heading"
+          >
+            B
+          </Button>
+          <Button
+            variant={editorState?.isItalic ? "default" : "secondary"}
+            className="italic border"
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            aria-label="Heading"
+          >
+            I
+          </Button>
+          <Button
+            variant={editorState?.isStrike ? "default" : "secondary"}
+            className="line-through border"
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            aria-label="Heading"
+          >
+            S
+          </Button>
+        </ButtonGroup>
+      </ButtonGroup>
+    </FloatingMenuApi>
+  );
+}
+
+export { FloatingMenu };
