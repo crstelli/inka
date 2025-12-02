c74e409cd6672349b4a4328fc5cd6f41bfd1303e
Giuseppe <cresctelli@gmail.com>
2025-12-02 08:28:31 +0100

    Displayed a message if serach produces no results

--- components/Notes.tsx
+++ components/Notes.tsx
@@ -10,11 +10,10 @@ function Notes() {
   const notes = useNotes();
   const search = useSearch();
 
-  if (notes.length === 0) return <NotePlaceholder />;
-
   let filteredNotes = notes;
 
   if (search.length > 0) filteredNotes = notes.filter((n) => n.title.toLowerCase().includes(search.toLowerCase()));
+  if (filteredNotes.length === 0) return <NotePlaceholder />;
 
   return (
     <div className="p-4 flex flex-col gap-4 overflow-auto">
