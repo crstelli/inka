b13c8badc6e400acb61343c3c16cf60dc5465e5d
Giuseppe <cresctelli@gmail.com>
2025-12-02 08:40:15 +0100

    Implemented pagination state managment in search store

--- stores/searchStore.ts
+++ stores/searchStore.ts
@@ -2,14 +2,26 @@ import { create } from "zustand";
 
 interface SearchState {
   search: string;
+  page: number;
 
   setSearch: (query: string) => void;
+  pageNext: () => void;
+  pagePrev: () => void;
 }
 
 const useSearchStore = create<SearchState>((set) => ({
   search: "",
+  page: 1,
+
   setSearch: (query) => set(() => ({ search: query })),
+
+  pageNext: () => set((state) => ({ page: state.page + 1 })),
+  pagePrev: () => set((state) => ({ page: state.page - 1 })),
 }));
 
 export const useSearch = () => useSearchStore((state) => state.search);
 export const useSetSearch = () => useSearchStore((state) => state.setSearch);
+
+export const usePage = () => useSearchStore((state) => state.page);
+export const usePageNext = () => useSearchStore((state) => state.pageNext);
+export const usePagePrev = () => useSearchStore((state) => state.pagePrev);
