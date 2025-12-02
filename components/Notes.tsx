d4064f9ab0755bbd6ef50dd4024093a77b4ee5ec
Giuseppe <cresctelli@gmail.com>
2025-12-02 08:25:38 +0100

    Implemented search filter in notes list

--- components/Notes.tsx
+++ components/Notes.tsx
@@ -1,16 +1,28 @@
 "use client";
 
 import { useNotes } from "@/stores/notesStore";
+import { useSearch } from "@/stores/searchStore";
 
 import { Note } from "@/components/Note";
 import { NotePlaceholder } from "@/components/NotePlaceholder";
 
 function Notes() {
   const notes = useNotes();
+  const search = useSearch();
+
+  console.log(notes);
+
+  if (notes.length === 0) return <NotePlaceholder />;
+
+  let filteredNotes = notes;
+
+  if (search.length > 0) filteredNotes = notes.filter((n) => n.title.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <div className="p-4 flex flex-col gap-4 overflow-auto">
-      {notes.length > 0 ? notes.map((n) => <Note key={n.id} note={n} />) : <NotePlaceholder />}
+      {filteredNotes.map((n) => (
+        <Note key={n.id} note={n} />
+      ))}
     </div>
   );
 }
