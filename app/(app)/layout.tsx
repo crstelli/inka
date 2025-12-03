0c0581df81b1f00e98885003fcf8b29f93d8ec03
Giuseppe <cresctelli@gmail.com>
2025-12-03 11:51:10 +0100

    Layout bug fix

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en" className={`dark ${geist.className}`}>
       <body className="w-screen h-screen bg-background text-foreground">
-        <main className="grid grid-cols-[auto_10fr] grid-rows-1 divide-x divide-border">
+        <main className="w-full h-full grid grid-cols-[auto_10fr] grid-rows-1 divide-x divide-border">
           <Sidebar />
           {children}
         </main>
