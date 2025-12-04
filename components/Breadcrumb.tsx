2498a9240e242101a9cdbdafb7c83ea906d822fe
Giuseppe <cresctelli@gmail.com>
2025-12-04 20:47:05 +0100

    Implemented optional rendering for autosaving message spinner.

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { useIsSaving } from "@/stores/notesStore";
+
 import { Spinner } from "@/components/Spinner";
 import { Breadcrumb as BreadcrumbComp, BreadcrumbItem, BreadcrumbList, BreadcrumbSeparator } from "./ui/breadcrumb";
 
 function Breadcrumb() {
+  const isSaving = useIsSaving();
+
   return (
     <div className="flex items-center justify-between px-4 [grid-area:breadcrumb]">
       <BreadcrumbComp>
@@ -11,10 +17,12 @@ function Breadcrumb() {
           <BreadcrumbItem>Welcome to Inka</BreadcrumbItem>
         </BreadcrumbList>
       </BreadcrumbComp>
-      <span className="text-secondary flex items-center gap-2">
-        <Spinner size="size-4" />
-        Autosaving
-      </span>
+      {isSaving && (
+        <span className="text-secondary flex items-center gap-2">
+          <Spinner size="size-4" />
+          Autosaving
+        </span>
+      )}
     </div>
   );
 }
