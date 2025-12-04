0a1aa5e3d48395aed593db8a72c165bf9805dbe8
Giuseppe <cresctelli@gmail.com>
2025-12-04 21:07:17 +0100

    Implemented select component from shadcn/ui.

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -1,3 +1,5 @@
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
 export default function page() {
   return (
     <main className="flex flex-col items-center py-20">
@@ -5,7 +7,20 @@ export default function page() {
       <h2 className="text-lg text-secondary font-light">Adjust the app settings to match your preferences.</h2>
       <div className="mt-20 grid grid-cols-[1fr_auto]">
         <h3 className="text-xl">Theme</h3>
-        <div className="row-span-2"></div>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
         <h4 className="text-secondary">Change the theme of the app</h4>
       </div>
     </main>
