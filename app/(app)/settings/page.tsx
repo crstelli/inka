5280113d05e206c1dd5fdbeede86f7f9dc236e43
Giuseppe <cresctelli@gmail.com>
2025-12-04 21:02:14 +0100

    Built settings page.

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -1,3 +1,7 @@
 export default function page() {
-  return <div>settings</div>;
+  return (
+    <main className="flex flex-col items-center py-20">
+      <h1 className="text-2xl">Settings</h1>
+    </main>
+  );
 }
