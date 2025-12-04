80794267777aeaf69fe40fd52dfa05e140acdf43
Giuseppe <cresctelli@gmail.com>
2025-12-04 21:04:28 +0100

    Implemented settings page layout

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -1,7 +1,13 @@
 export default function page() {
   return (
     <main className="flex flex-col items-center py-20">
-      <h1 className="text-2xl">Settings</h1>
+      <h1 className="text-2xl font-bold text-primary">Settings</h1>
+      <h2 className="text-lg text-secondary font-light">Adjust the app settings to match your preferences.</h2>
+      <div className="mt-20 grid grid-cols-[1fr_auto]">
+        <h3 className="text-xl">Theme</h3>
+        <div className="row-span-2"></div>
+        <h4 className="text-secondary">Change the theme of the app</h4>
+      </div>
     </main>
   );
 }
