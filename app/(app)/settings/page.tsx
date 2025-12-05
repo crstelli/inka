1940a0ff943e0a6029ea0ea03216916ef9c02d26
Giuseppe <cresctelli@gmail.com>
2025-12-05 08:07:33 +0100

    Fixed semantic html tag.

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -2,7 +2,7 @@ import { ThemeSelector } from "@/components/ThemeSelector";
 
 export default function page() {
   return (
-    <main className="flex flex-col items-center py-20 w-[50%] mx-auto">
+    <section className="flex flex-col items-center py-20 w-[50%] mx-auto">
       <h1 className="text-2xl font-bold text-primary">Settings</h1>
       <h2 className="text-lg text-secondary font-light">Adjust the app settings to match your preferences.</h2>
       <div className="mt-20 grid grid-cols-[1fr_auto] items-center w-full">
@@ -12,6 +12,6 @@ export default function page() {
         </div>
         <h4 className="text-secondary">Change the theme of the app</h4>
       </div>
-    </main>
+    </section>
   );
 }
