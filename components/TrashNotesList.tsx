b5ea7674d1571bc20b081935905054d8f63751c4
Giuseppe <cresctelli@gmail.com>
2025-12-05 09:15:23 +0100

    Added a message for empty trash.

--- components/TrashNotesList.tsx
+++ components/TrashNotesList.tsx
@@ -8,9 +8,11 @@ function TrashNotesList() {
 
   return (
     <div className="mt-20 flex flex-col w-full max-w-[500px]">
-      {trashNotes.map((n) => (
-        <TrashNote note={n} key={n.id} />
-      ))}
+      {trashNotes.length > 0 ? (
+        trashNotes.map((n) => <TrashNote note={n} key={n.id} />)
+      ) : (
+        <p className="text-center">The trash is empty.</p>
+      )}
     </div>
   );
 }
