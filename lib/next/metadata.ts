dbadcb33ea728e66b8fbe92ce68df6b6056e7b95
Giuseppe <cresctelli@gmail.com>
2025-12-05 09:29:46 +0100

    Dynamic favicon theme based on system theme.

--- lib/next/metadata.ts
+++ lib/next/metadata.ts
@@ -4,4 +4,18 @@ export const metadata: Metadata = {
   title: "Inka - Note Taking App",
   description:
     "Inka is a fast, lightweight note-taking app designed for clarity and focus. It lets users create, edit, and organize notes with a clean text-editor experience inspired by modern knowledge tools. Inka stores content locally for privacy, supports quick search, and offers an intuitive UI that adapts to different workflows. The goal is to provide a streamlined space to capture ideas, draft documents, and manage personal knowledge without distractions.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicons/icon-dark.ico",
+        href: "/favicons/icon-dark.ico",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicons/icon-light.ico",
+        href: "/favicons/icon-light.ico",
+      },
+    ],
+  },
 };
