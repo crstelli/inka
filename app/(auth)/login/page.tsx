dd9631889c038722643fd414099e44e6349149c8
Giuseppe <cresctelli@gmail.com>
2025-12-08 12:00:27 +0100

    Implemented Google login.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Field, FieldSet } from "@/components/ui/field";
-import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
-import { KeyRound, Mail } from "lucide-react";
 import Link from "next/link";
+import { signInWithGoogle } from "@/actions/signInWithGoogle";
+
+import { Button } from "@/components/ui/button";
+import { Field, FieldSet } from "@/components/ui/field";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+
+import { KeyRound, Mail } from "lucide-react";
 
 export default function page() {
   return (
@@ -12,46 +15,51 @@ export default function page() {
         <h1 className="text-4xl">Inka</h1>
         <h2 className="text-secondary">A modern taking-note app.</h2>
       </div>
-      <form className="flex flex-col">
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-center text-lg">Login into your account</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <FieldSet className="gap-4">
-              <Field>
-                <InputGroup>
-                  <InputGroupInput placeholder="Your email" type="email" />
-                  <InputGroupAddon>
-                    <Mail />
-                  </InputGroupAddon>
-                </InputGroup>
-              </Field>
-              <Field>
-                <InputGroup>
-                  <InputGroupInput placeholder="Your password" type="password" />
-                  <InputGroupAddon>
-                    <KeyRound />
-                  </InputGroupAddon>
-                </InputGroup>
-              </Field>
-            </FieldSet>
-            <FieldSet className="mt-6">
-              <Field>
-                <Button asChild>
-                  <Link href="/">Login</Link>
-                </Button>
-              </Field>
-            </FieldSet>
-          </CardContent>
-        </Card>
-        <span className="text-center mx-auto mt-2">
-          You don&apos;t have an account?{" "}
-          <Link href="/signup" className="underline">
-            Sign Up
-          </Link>
-        </span>
-      </form>
+      <div className="flex flex-col">
+        <form className="flex flex-col">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-center text-lg">Login into your account</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <FieldSet className="gap-4">
+                <Field>
+                  <InputGroup>
+                    <InputGroupInput placeholder="Your email" type="email" />
+                    <InputGroupAddon>
+                      <Mail />
+                    </InputGroupAddon>
+                  </InputGroup>
+                </Field>
+                <Field>
+                  <InputGroup>
+                    <InputGroupInput placeholder="Your password" type="password" />
+                    <InputGroupAddon>
+                      <KeyRound />
+                    </InputGroupAddon>
+                  </InputGroup>
+                </Field>
+              </FieldSet>
+              <FieldSet className="mt-6">
+                <Field>
+                  <Button asChild>
+                    <Link href="/">Login</Link>
+                  </Button>
+                </Field>
+              </FieldSet>
+            </CardContent>
+          </Card>
+          <span className="text-center mx-auto mt-2">
+            You don&apos;t have an account?{" "}
+            <Link href="/signup" className="underline">
+              Sign Up
+            </Link>
+          </span>
+        </form>
+        <form className="mx-auto mt-4" action={signInWithGoogle}>
+          <Button type="submit">Login with Google</Button>
+        </form>
+      </div>
     </>
   );
 }
