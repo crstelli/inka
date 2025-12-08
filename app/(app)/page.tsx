75175bdfbc2cb54de0c31bca33c2cd06a6a010b9
Giuseppe <cresctelli@gmail.com>
2025-12-08 07:45:51 +0100

    Getting users from the database.

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,16 +1,20 @@
 // import { useEffect } from "react";
 // import { useLoadNotes } from "@/stores/notesStore";
+import prisma from "@/lib/prima";
 
 import { NotesMenu } from "@/components/NotesMenu";
 import { Heading } from "@/components/Heading";
 import { EditorSection } from "@/components/EditorSection";
 
-export default function Page() {
+export default async function Page() {
   // const loadNotes = useLoadNotes();
 
   // useEffect(() => {
   //   loadNotes();
   // }, [loadNotes]);
+
+  const users = await prisma.user.findMany();
+  console.log(users);
 
   return (
     <div className="grid grid-cols-[3fr_10fr] divide-solid divide-x divide-y grid-rows-[50px_50px_1fr_50px] layout">
