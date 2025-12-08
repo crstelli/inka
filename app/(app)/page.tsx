865d2f9027d6e276306da2a9e5e029bce87f4c7f
Giuseppe <cresctelli@gmail.com>
2025-12-08 07:42:12 +0100

    Made the main page server component to integrate Prisma.

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,18 +1,16 @@
-"use client";
-
-import { useEffect } from "react";
-import { useLoadNotes } from "@/stores/notesStore";
+// import { useEffect } from "react";
+// import { useLoadNotes } from "@/stores/notesStore";
 
 import { NotesMenu } from "@/components/NotesMenu";
 import { Heading } from "@/components/Heading";
 import { EditorSection } from "@/components/EditorSection";
 
 export default function Page() {
-  const loadNotes = useLoadNotes();
+  // const loadNotes = useLoadNotes();
 
-  useEffect(() => {
-    loadNotes();
-  }, [loadNotes]);
+  // useEffect(() => {
+  //   loadNotes();
+  // }, [loadNotes]);
 
   return (
     <div className="grid grid-cols-[3fr_10fr] divide-solid divide-x divide-y grid-rows-[50px_50px_1fr_50px] layout">
