2f553d10515f86406f6b710b2f0aae238bd25a4b
Giuseppe <cresctelli@gmail.com>
2025-12-11 08:57:37 +0100

    Hide notes list in small devices.

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -4,8 +4,8 @@ import { EditorSection } from "@/components/EditorSection";
 
 export default async function page() {
   return (
-    <div className="grid grid-cols-[300px_1fr] grid-rows-[50px_1fr] divide-x">
-      <NotesSection />
+    <div className="grid grid-cols-1 lg:grid-cols-[300px_1fr] grid-rows-[50px_1fr] divide-x">
+      <NotesSection className="max-lg:hidden" />
       <Heading />
       <EditorSection />
     </div>
--- components/NotesSection.tsx
+++ components/NotesSection.tsx
@@ -5,11 +5,15 @@ import { NotesSearch } from "@/components/NotesSearch";
 import { NotesList } from "@/components/NotesList";
 import { Pagination } from "@/components/Pagination";
 
-async function NotesSection() {
+interface Props {
+  className?: React.ComponentProps<"section">["className"];
+}
+
+async function NotesSection({ className }: Props) {
   const notes = await getNotes();
 
   return (
-    <section className="max-lg:hidden row-span-2 grid grid-cols-1 grid-rows-[50px_50px_1fr_50px] divide-y">
+    <section className={`row-span-2 grid grid-cols-1 grid-rows-[50px_50px_1fr_50px] divide-y ${className}`}>
       <NotesHeading count={notes.length} />
       <NotesSearch />
       <NotesList notes={notes} />
