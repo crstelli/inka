b4eebb5053c1b586a33330289cdc9440b781a271
Giuseppe <cresctelli@gmail.com>
2025-12-11 10:38:06 +0100

    Fixed noteslist size.

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -9,7 +9,7 @@ export default async function page() {
   const notes = await getNotes();
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[300px_1fr] grid-rows-[50px_1fr] divide-x">
+    <div className="grid grid-cols-1 lg:grid-cols-[400px_1fr] grid-rows-[50px_1fr] divide-x">
       <NotesSection notes={notes} className="max-lg:hidden" />
       <Heading />
       <EditorSection />
